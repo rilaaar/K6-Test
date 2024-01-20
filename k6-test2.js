14dd088ac8da144f936e45353a8eb316b48b5ef7
Rila Afhrila <rilaafhrila15@gmail.com>
2024-01-20 20:42:25 +0700

    update data

--- k6-test2.js
+++ k6-test2.js
@@ -3,8 +3,8 @@ import http from 'k6/http';
 export default function () {
   const url = 'http://test.k6.io/login';
   const payload = JSON.stringify({
-    email: 'aaa',
-    password: 'bbb',
+    email: 'rila@mail.com',
+    password: '12345678',
   });
 
   const params = {

f3d629ada63f9b7b9c038b82abba1e7e35539919
Rila Afhrila <rilaafhrila15@gmail.com>
2024-01-23 18:25:29 +0700

    add check status

--- createUser_reqres-in.js
+++ createUser_reqres-in.js
@@ -1,7 +1,22 @@
 import http from 'k6/http';
+import { check } from 'k6';
+
+
+export const options = {
+  scenarios: {
+    cretae_users: {
+      executor: 'constant-arrival-rate',
+      duration: '5s', // total duration
+      preAllocatedVUs: 10, // to allocate runtime resources     preAll
+
+      rate: 35, // number of constant iterations given `timeUnit`
+      timeUnit: '2s',
+    },
+  },
+};
 
 export default function () {
-  const url = 'https://reqres.in/api/users';
+//  const url = 'https://reqres.in/api/users';
   const payload = JSON.stringify({
     name: 'morpheus',
     job: 'leader',
@@ -13,5 +28,14 @@ export default function () {
     },
   };
 
-  http.post(url, payload, params);
+  const res = http.post('https://reqres.in/api/users', payload, params);
+
+  check(res, {
+    'Post status is 201': (r) => res.status === 201,
+  //  'Post Content-Type header': (r) => res.headers['Content-Type'] === 'application/json',
+  //  'Post response name': (r) => res.status === 201 && res.json().json.name === 'morpheus',
+  });
 }
+
+
+// k6 run createUser_reqres-in.js

d8b3bba015d0c99cd0e48a005ff789871eda5735
Rila Afhrila <rilaafhrila15@gmail.com>
2024-01-23 20:41:53 +0700

    update script

--- createUser_reqres-in.js
+++ createUser_reqres-in.js
@@ -6,10 +6,10 @@ export const options = {
   scenarios: {
     cretae_users: {
       executor: 'constant-arrival-rate',
-      duration: '5s', // total duration
-      preAllocatedVUs: 2, // to allocate runtime resources     preAll
+      duration: '10s', // total duration
+      preAllocatedVUs: 1000, // to allocate runtime resources     preAll
 
-      rate: 5, // number of constant iterations given `timeUnit`
+      rate: 3500, // number of constant iterations given `timeUnit`
       timeUnit: '2s',
     },
   },
@@ -22,20 +22,20 @@ export default function () {
     job: 'leader',
   });
 
-//  const params = {
-//    headers: {
-//      'Content-Type': 'application/json',
-//    },
-//  }; 
+  const params = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }; 
 
-  const headers = { 'Content-Type': 'application/json' };
+//  const headers = { 'Content-Type': 'application/json' };
 
-  const res = http.post('https://reqres.in/api/users', payload, {headers});
+  const res = http.post('https://reqres.in/api/users', payload, params);
 
   check(res, {
-    'Post status is 201': (r) => res.status === 201,
-    'Post Content-Type header': (r) => res.headers['Content-Type'] === 'application/json',
-    'Post response name': (r) => res.status === 201 && res.json().json.name === 'morpheus',
+    'Verify Post status is 201': (r) => res.status === 201,
+//    'Post Content-Type header': (r) => res.headers['Content-Type'] === 'application/json',
+//    'Post response name': (r) => res.status === 201 && res.json().json.name === 'morpheus',
   });
 }
 
